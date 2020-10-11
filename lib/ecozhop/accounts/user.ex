2070accba4396ea8bfa8904ef447bb2dbcbae84c
ricardorojass <reddrum7@gmail.com>
2020-10-11 00:18:06 -0500

    Feat: Create Accounts context with User migration

---
+++ lib/ecozhop/accounts/user.ex
@@ -0,0 +1,43 @@
+defmodule Ecozhop.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :name, :string
+    field :email, :string
+    field :password, :string, virtual: true
+    field :password_hash, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :email, :password])
+    |> validate_required([:name, :email, :password])
+    |> validate_format(:email, ~r/^[A-Za-z0-9._-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/)
+    |> validate_length(:name, min: 1, max: 30)
+    |> validate_length(:password, min: 6)
+    |> unique_constraint(:email)
+    |> put_pass_hash
+  end
+
+  def registration_changeset(user, params) do
+    user
+    |> changeset(params)
+    |> cast(params, [:password], [])
+    |> validate_length(:password, min: 6, max: 100)
+    |> put_pass_hash()
+  end
+
+  defp put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}}
+        ->
+          put_change(changeset, :password_hash, Pbkdf2.hash_pwd_salt(pass))
+      _ ->
+          changeset
+    end
+  end
+end
---
+++ lib/ecozhop_web/views/user_view.ex
@@ -0,0 +1,19 @@
+defmodule EcozhopWeb.UserView do
+  use EcozhopWeb, :view
+  alias EcozhopWeb.UserView
+
+  def render("index.json", %{users: users}) do
+    %{data: render_many(users, UserView, "user.json")}
+  end
+
+  def render("show.json", %{user: user}) do
+    %{data: render_one(user, UserView, "user.json")}
+  end
+
+  def render("user.json", %{user: user}) do
+    %{id: user.id,
+      name: user.name,
+      email: user.email,
+      encrypted_password: user.encrypted_password}
+  end
+end
